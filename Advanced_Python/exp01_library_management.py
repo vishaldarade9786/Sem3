48b955f5c035e0e16b25a3a151f96e601cf5ef6f
Vishal <vishaldarade9786@gmail.com>
2026-07-17 18:55:28 +0530

    feat: added CLI menu with error handling and exit routine.

--- Advanced_Python/exp01_library_management.py
+++ Advanced_Python/exp01_library_management.py
@@ -3,7 +3,6 @@ class Book:
         self.title=title
         self.book_id = book_id
         self.book_availability = book_availability
-    
 
 class Patron:
     def __init__(self,name,user_id):
@@ -22,10 +21,12 @@ class Library:
         # The library just catches the book and puts it on the shelf.
         # No inputs, no prints. Pure logic.
         self.books.append(new_book)
+        return "Book added Successfully"
 
     def register_patron(self, new_patron):
         # Hard lesson , This is only brain not ears and eyes or mouth.
         self.patrons.append(new_patron)
+        return "Patron registered Successfully"
 
     def borrow_book(self,patron_id,target_book_id):
         # check if given values exist in database or not
@@ -76,3 +77,31 @@ class Library:
             temp_book.book_availability = True
             return "Book successfully returned"
         
+my_college_library = Library()
+while True:
+    print("---WELCOME TO LIBRARY--- \nChoose one of the following service by entering the serial number")
+
+    try:
+        choice = int(input("1.Add Book\n2.Register Patron\n3.Borrow Book\n4.Return Book\n5.exit"))
+        if choice == 1:
+            fresh_book = Book(input("Enter the title of Book: "),int(input("Enter Book ID: ")))
+            print(my_college_library.add_book(fresh_book))
+
+        elif choice == 2:
+            fresh_patron = Patron(input("Enter User name: "),int(input("User ID: ")))
+            print(my_college_library.register_patron(fresh_patron))
+        
+        elif choice == 3:
+            print(my_college_library.borrow_book(int(input("Enter Patron ID: ")),int(input("Enter book ID of book you want to issue: "))))
+
+        elif choice == 4:
+            print(my_college_library.return_book(int(input("Enter Patron ID: ")),int(input("Enter book ID of book you want to return: "))))
+        
+        elif choice == 5:
+            break
+
+        else:
+            print("Error: Please enter valid service number.")
+
+    except ValueError:
+        print("Error:Please Enter valid input")
