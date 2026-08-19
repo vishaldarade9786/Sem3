88264519c2e2eb6ce9922b0eefa6dee229856b12
Vishal <vishaldarade9786@gmail.com>
2026-08-19 18:42:29 +0530

    feat: implement DP Tabulation and Memoization algorithms
    Completed Ass4 for Unit 2. Built (N)$ dynamic programming solutions to compute the nth Fibonacci number with a robust CLI.

--- Advanced_Python/Fibonacci_Series.py
+++ Advanced_Python/Fibonacci_Series.py
@@ -1,26 +1,45 @@
 # Fibonacci series is a series in which the nubmer is sum of previous two numbers
-def Find_Fibonacci(n):
+def Tabulation_Fibonacci(n):
     fibonacci_seq = [0,1]
     if n <= 0:
-        return []
+        return 0
     if n == 1:
-        return [0]
-    while len(fibonacci_seq) < n:
-        fibonacci_seq.append(fibonacci_seq[-1]+fibonacci_seq[-2])
-    return fibonacci_seq
+        return 1
+    while len(fibonacci_seq) <= n:
+        fibonacci_seq.append(fibonacci_seq[-1] + fibonacci_seq[-2])
+    return fibonacci_seq[-1]
+
+def Memoization_Fibonacci(n , memo=None):
+    if memo is None:
+        memo = {}
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n in memo:
+        return memo[n]
+    elif n not in memo:
+        ans = int(Memoization_Fibonacci(n-1,memo)) + int(Memoization_Fibonacci(n-2,memo))
+        memo[n] = ans
+        return memo[n]
+    
 if __name__ == "__main__":
     while True:
         print("--- Menu ---")
-        print("1.Find fibonacci sequence for custom number ")
-        print("2.exit")
+        print("1.Find Fibonacci sequence through Tabulation")
+        print("2.Find Fibonacci sequence through Memoization")
+        print("3.exit")
         try: 
             choice = int(input("Enter your choice :"))
-            if choice == 2:
+            if choice == 3:
                 print(f"Exiting Gracefully")
                 break
+            elif choice == 2:
+                n = int(input("Enter n :"))
+                print(Memoization_Fibonacci(n))
             elif choice == 1:
                 n = int(input("Enter n :"))
-                print(Find_Fibonacci(n))
+                print(Tabulation_Fibonacci(n))
             else:
                 print("Enter valid Choice!!!")
         except ValueError:
